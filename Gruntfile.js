448bf258aed886220a7c7f03ee6b79ebb8f10433
Jobayer Arman <carbonjha@gmail.com>
2015-12-08 18:40:43 +0600

    update 0.3.0
    - folder restructured
    - package update

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,20 @@ module.exports = function(grunt) {
 
     // Remove built directory
     clean: {
-      build: ['dist/']
+      html: ['dist/index.html', 'dist/pages/*.html'],
+      css : ['dist/assets/css/']
+    },
+
+    // Built stylesheets with less
+    less: {
+      build: {
+        options: {
+          paths: ["assets/css"]
+        },
+        files: {
+          "dist/assets/css/main.css": "src/less/*.less"
+        }
+      }
     },
 
     // Build the site using grunt-includes
@@ -16,18 +29,36 @@ module.exports = function(grunt) {
         dest: 'dist/',
         options: {
           flatten: true,
-          includePath: 'include'
+          includePath: 'site/include'
         }
+      }
+    },
+
+    watch: {
+      html: {
+        options: {
+          spawn: false
+        },
+        files: ['site/*.html', 'site/pages/*.html'],
+        tasks: ['clean:html', 'includes']
+      },
+      styles: {
+        options: {
+          spawn: false
+        },
+        files: ['src/less/*.less'],
+        tasks: ['clean:css', 'less']
       }
     }
   });
 
   // Load plugins used by this task gruntfile
   grunt.loadNpmTasks('grunt-includes');
-  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Task definitions
-  grunt.registerTask('build', ['clean', 'less', 'includes']);
-  grunt.registerTask('default', ['build']);
+  grunt.registerTask('build', ['clean', 'includes']);
+  grunt.registerTask('default', ['watch']);
 };
